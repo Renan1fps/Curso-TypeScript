ad12eeb25071c240c076b88a394f80c286f300ab
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-16 14:52:45 -0300

    Any, void, never para tipagem de funções

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,45 @@
-let message: string = "hello wold"; //para tipagem de variavél 
+let message: string = "hello wold"; //para tipagem de variavél
 
 console.log(message);
 
-let total: number;//para tipagem de variaveis numericas podendo ser com virgula
+let total: number; //para tipagem de variaveis numericas podendo ser com virgula
 total = 10.5;
 
-let isOpen: boolean; 
+let isOpen: boolean;
 isOpen = false;
+
+//any, void, never
+
+//any vai aceitar qualquer valor (string, boolean,number) *não aconselhavel
+
+let stock: any = 89;
+stock = "null";
+
+//void geralmente usado para função que não retorna nada
+
+//nesse caso setei a função com o tipo do retorno que quero
+function showInformation(): string {
+  return "cataline";
+}
+//aqui ele não me retorna nada e eu assinalo o void no metodo
+function showInformation2(): void {
+  console.log("ola");
+}
+//no caso de promises é só colocar o tipo de retorno dentro de <>
+//function showInformation3(): Promise<string> {
+//   console.log("ola");
+//  }
+
+
+//em casos de parametros é só colocar o tipo ao lado
+function showInformation4(name: string): void {
+    console.log("ola");
+  }
+
+//never quase nunca usado
+//usado para erros, exeçoes ou loops
+function showInformation5(name: string):never {
+   throw new Error("Error")
+  }
+
+  
