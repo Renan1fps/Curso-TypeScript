68880b6e80f1e3e878c11c589216e39464cce456
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-16 14:38:39 -0300

    Tipando variaveis

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello word! teste")
+let message: string = "hello wold"; //para tipagem de variavél 
+
+console.log(message);
+
+let total: number;//para tipagem de variaveis numericas podendo ser com virgula
+total = 10.5;
+
+let isOpen: boolean; 
+isOpen = false;
