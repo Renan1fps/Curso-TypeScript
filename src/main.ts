463c28fa0dd8dd348c239bde90723adaf0a3d810
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-16 15:11:17 -0300

    Arrays, Tuple e Enum

--- src/main.ts
+++ src/main.ts
@@ -30,16 +30,41 @@ function showInformation2(): void {
 //   console.log("ola");
 //  }
 
-
 //em casos de parametros é só colocar o tipo ao lado
 function showInformation4(name: string): void {
-    console.log("ola");
-  }
+  console.log("ola");
+}
 
 //never quase nunca usado
 //usado para erros, exeçoes ou loops
-function showInformation5(name: string):never {
-   throw new Error("Error")
-  }
+function showInformation5(name: string): never {
+  throw new Error("Error");
+}
 
-  
+//array, tuple, enum
+
+//Arrays= preciso passar o tipo dentro de <> para que ele saiba qual tipo de dados o array recebera e dara erro caso tente colocar uum diferente ex:
+//mas essa maneira não é muito usada
+let precos: Array<number>;
+precos = [1, 2, 3, 4, 5, 6];
+
+//maneira mais usada ex:
+let valores: number[];
+valores = [1, 2, 3, 4, 5, 6];
+
+//Tuple
+//Um array quando sabemos a quantidade de itens que teremos e o tipo de cada um
+let itens: [number, string]
+itens=[1,"hello"]
+
+//Enum
+//é um conjunto de chaves e valores ex:
+enum Colors{
+    white="#fff",
+    black= "#000"
+}
+
+//ex de uso: quero uma cor mas não lembro seu valor, então é só fazer assim:
+
+let corWhite: Colors= Colors.black
+console.log(corWhite)
