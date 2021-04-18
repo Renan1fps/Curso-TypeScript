d22e60398e8d2de7e106b4c48bd5345df0cbbfe6
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-18 14:41:46 -0300

    Tipos de parâmetros

--- back-end-Node.js/src/server.ts
+++ back-end-Node.js/src/server.ts
@@ -1,9 +1,21 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/users", (request, response) => {
   return response.json({ message: "Pegando usuarios" });
+});
+
+app.get("/users/:id", (request, response) => {
+    const id = request.query;
+    console.log(id)
+    return response.json("finish")
+  });
+
+app.post("/users/1", (request, response) => {
+  const { id, name, age } = request.body;
+  return response.json({ id: id, name: name, age: age });
 });
 
 app.post("/users", (request, response) => {
