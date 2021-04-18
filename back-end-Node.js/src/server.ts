52bc15c03c567cbe94050761c6bc9dc1f24c5097
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-18 14:03:05 -0300

    Métodos HTTP

--- back-end-Node.js/src/server.ts
+++ back-end-Node.js/src/server.ts
@@ -2,11 +2,20 @@ import express, { response } from "express";
 
 const app = express();
 
-app.get("/users", (require, response)=>{
-    response.json({message: "Hello!"})
-})
+app.get("/users", (request, response) => {
+  return response.json({ message: "Pegando usuarios" });
+});
 
-app.listen(8080,()=>{
-    console.log("Back-end is running")
-})
+app.post("/users", (request, response) => {
+  return response.json({ message: "Criando usuario" });
+});
+app.put("/users", (request, response) => {
+  return response.json({ message: "Atualizando usuario" });
+});
+app.delete("/users", (request, response) => {
+  return response.json({ message: "Deletenado usuario" });
+});
 
+app.listen(8080, () => {
+  console.log("Back-end is running");
+});
